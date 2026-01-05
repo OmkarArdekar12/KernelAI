ba0796bf28dec09d7fdf0282be278f442a9c6694
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-05 17:35:23 +0530

    Global Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import MatrixBackground from "./components/MatrixBackground";
 
 export default function Home() {
   return (
-    <main className="w-full relative min-h-screen overflow-hidden bg-black-600 text-white">
+    <main className="w-full h-full min-w-full relative min-h-screen bg-black text-white">
       <MatrixBackground />
-      <div className="relative z-10 flex flex-column w-full items-center justify-center py-6">
+      <div className="relative z-10 flex flex-column w-full items-center justify-center p-10">
         <Header />
       </div>
     </main>
