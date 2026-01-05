51a6d42f7db9b4472f9ec9a635f53ccd58eefc12
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-05 20:34:10 +0530

    Header Completed

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import KernelAIBrand from "./KernelAIBrand";
 import TypewriterText from "./TypewriterText";
--- src/app/components/MainContent.tsx
+++ src/app/components/MainContent.tsx
@@ -4,21 +4,40 @@ import React, { useState } from "react";
 import HistoryPanel from "./HistoryPanel";
 import tabs from "../data/tabs";
 import { HistoryItem, Tab } from "../types";
+import CodeExplanation from "./CodeExplanation";
+import CodeDebugging from "./CodeDebugging";
+import CodeGeneration from "./CodeGeneration";
 
 const MainContent = () => {
   const [activeTab, setActiveTab] = useState<Tab["id"]>("explain");
   const [history, setHistory] = useState<HistoryItem[]>([]);
+
+  const addToHistory = (
+    type: HistoryItem["type"],
+    input: string,
+    output: string
+  ) => {
+    const newItem: HistoryItem = {
+      id: Date.now(),
+      type,
+      timestamp: new Date().toLocaleString(),
+      input,
+      output,
+    };
+    setHistory((prev) => [newItem, ...prev.slice(0, 9)]);
+  };
+
   return (
-    <div className="w-full flex flex-col items-center justify-center lg:flex-row gap-8 py-2 px-4 md:px-10">
+    <div className="w-full flex flex-col items-center justify-center lg:flex-row gap-8 py-2 px-1 sm:px-4 md:px-10">
       <div className="w-full lg:2/3 flex flex-col">
         <div className="w-full bg-black-800/50 backdrop-blur-xl rounded-2xl shadow-2xl border border-emerald-600/95">
-          <div className="w-full flex flex-wrap items-center gap-3 border-b-emerald-500 border-black-700/50 bg-black-900-50 p-2">
+          <div className="w-full flex flex-wrap items-center gap-3 border-b border-emerald-600/95 border-black-700/50 bg-black-900-50 p-2">
             {tabs.map((tab: Tab) => {
               const Icon = tab.icon;
               return (
                 <button
                   key={tab.id}
-                  className={`flex items-center gap-2 px-6 py-3 rounded-lg font-semibold transition-all cursor-pointer ${
+                  className={`flex flex-wrap items-center gap-2 px-6 py-3 rounded-lg font-semibold transition-all cursor-pointer ${
                     activeTab == tab.id
                       ? `bg-linear-to-r ${tab.gradient} text-white shadow-lg`
                       : "text-gray-400 hover:text-white hover:bg-gray-700/50"
@@ -33,9 +52,20 @@ const MainContent = () => {
               );
             })}
           </div>
+          <div className="w-full flex items-center p-6">
+            {activeTab == "explain" && (
+              <CodeExplanation addToHistory={addToHistory} />
+            )}
+            {activeTab == "debug" && (
+              <CodeDebugging addToHistory={addToHistory} />
+            )}
+            {activeTab == "generate" && (
+              <CodeGeneration addToHistory={addToHistory} />
+            )}
+          </div>
         </div>
       </div>
-      <HistoryPanel />
+      <HistoryPanel history={history} />
     </div>
   );
 };
