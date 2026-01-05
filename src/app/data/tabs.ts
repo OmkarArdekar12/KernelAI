d62ce487f70b42d60baafc322b98b16189bab2d8
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-05 20:04:05 +0530

    Tabs Data

--- src/app/data/tabs.ts
+++ src/app/data/tabs.ts
@@ -1,22 +1,25 @@
 import { Tab } from "../types";
+import { FaFileCode } from "react-icons/fa";
+import { VscDebugAlt } from "react-icons/vsc";
+import { GiRegeneration } from "react-icons/gi";
 
 const tabs: Tab[] = [
   {
     id: "explain",
     label: "Explain Code",
-    icon: "🔍",
+    icon: FaFileCode,
     gradient: "from-purple-500 to-pink-500",
   },
   {
     id: "debug",
     label: "Debug Code",
-    icon: "🪲",
+    icon: VscDebugAlt,
     gradient: "from-red-500 to-orange-500",
   },
   {
     id: "generate",
     label: "Generate Code",
-    icon: "⚡",
+    icon: GiRegeneration,
     gradient: "from-green-500 to-blue-500",
   },
 ];
