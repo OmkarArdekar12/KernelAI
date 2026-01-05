7b6c396212af98a3f22b8c14612302486cc09bbc
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-05 17:32:35 +0530

    Global Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="w-full relative min-h-screen overflow-hidden bg-black-600 text-white">
       <MatrixBackground />
-      <div className="relative z-10 flex flex-column w-full items-center justify-center py-5">
+      <div className="relative z-10 flex flex-column w-full items-center justify-center py-6">
         <Header />
       </div>
     </main>
