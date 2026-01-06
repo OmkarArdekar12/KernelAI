4518fec2660b063e520b53106274e7dc9763b020
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-06 06:29:38 +0530

    Layout and SEO Completed

--- src/app/components/CodeExplanation.tsx
+++ src/app/components/CodeExplanation.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import { HistoryItem } from "../types";
 
-const CodeExplanation = () => {
+interface CodeExplanationProps {
+  addToHistory: (
+    type: HistoryItem["type"],
+    input: string,
+    output: string
+  ) => void;
+}
+
+const CodeExplanation = ({ addToHistory }: CodeExplanationProps) => {
+  const [code, setCode] = useState<string>("");
+  const [explanation, setExplanation] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleExplain = async () => {};
+
   return <div>CodeExplanation</div>;
 };
 
