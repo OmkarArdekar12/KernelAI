aac09cee37ad1143e9df8898d2b29b80ddd0b806
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-01-06 17:35:08 +0530

    Completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import ToasterMessage from "./components/ToasterMessage";
 export default function Home() {
   return (
     <div className="w-full min-w-full relative min-h-screen bg-black text-white transition-all duration-300">
-      {/* <MatrixBackground /> */}
+      <MatrixBackground />
       <div className="relative z-10 flex flex-col w-full min-h-screen items-center justify-center pt-6">
         <header className="flex w-full items-center justify-center">
           <nav className="flex w-full items-center justify-center">
