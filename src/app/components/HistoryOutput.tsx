8a4522cc14c8ef276af71249d5cace542874306a
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-02-19 23:15:33 +0530

    Completed

--- src/app/components/HistoryOutput.tsx
+++ src/app/components/HistoryOutput.tsx
@@ -5,6 +5,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import toast from "react-hot-toast";
 import { LuCopy, LuCheck } from "react-icons/lu";
+import rehypeHighlight from "rehype-highlight";
 
 interface HistoryOutputProps {
   outputType: string;
@@ -48,6 +49,7 @@ const HistoryOutput = ({ outputType, output }: HistoryOutputProps) => {
           <span>Copy All</span>
         </button>
         <ReactMarkdown
+          // rehypePlugins={[rehypeHighlight]}
           remarkPlugins={[remarkGfm]}
           components={{
             p({ children }) {
