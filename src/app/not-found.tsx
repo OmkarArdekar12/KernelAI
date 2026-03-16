83bad0f5d0266a91cb22c4aaeed7570195df01bc
OmkarArdekar12 <omkarardekar12@gmail.com>
2026-03-16 23:07:06 +0530

    Working on not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <Link href="/">Go KernelAI</Link>
+    </div>
+  );
+};
+
+export default NotFound;
